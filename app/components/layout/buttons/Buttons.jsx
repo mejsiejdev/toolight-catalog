d5097b4fe75df31479db909018f4fc360ad1becf
Krzysztof Tomaszewski <drteski@gmail.com>
2023-06-20 15:02:10 +0200

    initial

--- app/components/layout/buttons/Buttons.jsx
+++ app/components/layout/buttons/Buttons.jsx
@@ -1,37 +1,52 @@
 import "./styles/buttons.scss";
 
-const PrimaryButton = ({ actionOnClick, children }) => {
+const PrimaryButton = ({ actionOnClick, children, icon }) => {
 	return (
-		<button onClick={actionOnClick} className="primary-button">
-			{children}
+		<button onClick={actionOnClick} className="button button--primary">
+			<span className="button-content">
+				{icon && <span className="button-icon">{icon}</span>}
+				{children && <span className="button-title">{children}</span>}
+			</span>
 		</button>
 	);
 };
-const SecondaryButton = ({ actionOnClick, children }) => {
+const SecondaryButton = ({ actionOnClick, children, icon }) => {
 	return (
-		<button onClick={actionOnClick} className="secondary-button">
-			{children}
+		<button onClick={actionOnClick} className="button button--secondary">
+			<span className="button-content">
+				{icon && <span className="button-icon">{icon}</span>}
+				{children && <span className="button-title">{children}</span>}
+			</span>
 		</button>
 	);
 };
-const TertiaryButton = ({ actionOnClick, children }) => {
+const TertiaryButton = ({ actionOnClick, children, icon }) => {
 	return (
-		<button onClick={actionOnClick} className="tertiary-button">
-			{children}
+		<button onClick={actionOnClick} className="button button--tertiary">
+			<span className="button-content">
+				{icon && <span className="button-icon">{icon}</span>}
+				{children && <span className="button-title">{children}</span>}
+			</span>
 		</button>
 	);
 };
-const DangerButton = ({ actionOnClick, children }) => {
+const DangerButton = ({ actionOnClick, children, icon }) => {
 	return (
-		<button onClick={actionOnClick} className="danger-button">
-			{children}
+		<button onClick={actionOnClick} className="button button--danger">
+			<span className="button-content">
+				{icon && <span className="button-icon">{icon}</span>}
+				{children && <span className="button-title">{children}</span>}
+			</span>
 		</button>
 	);
 };
-const SuccessButton = ({ actionOnClick, children }) => {
+const SuccessButton = ({ actionOnClick, children, icon }) => {
 	return (
-		<button onClick={actionOnClick} className="success-button">
-			{children}
+		<button onClick={actionOnClick} className="button button--success">
+			<span className="button-content">
+				{icon && <span className="button-icon">{icon}</span>}
+				{children && <span className="button-title">{children}</span>}
+			</span>
 		</button>
 	);
 };
--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,14 @@
 import { PrimaryButton, SecondaryButton, TertiaryButton, DangerButton, SuccessButton } from "./components/layout/buttons/Buttons.jsx";
+import { AiFillApi } from "react-icons/ai";
 
 const HomePage = async () => {
 	return (
 		<>
-			<PrimaryButton>Dupa</PrimaryButton>
-			<SecondaryButton>Dupa</SecondaryButton>
-			<TertiaryButton>Dupa</TertiaryButton>
-			<DangerButton>Dupa</DangerButton>
-			<SuccessButton>Dupa</SuccessButton>
+			<PrimaryButton icon={<AiFillApi size={20} />}>Lorem ipsum dor somet!</PrimaryButton>
+			<SecondaryButton icon={<AiFillApi size={20} />}>Dupa</SecondaryButton>
+			<TertiaryButton icon={<AiFillApi size={20} />}>Dupa</TertiaryButton>
+			<DangerButton icon={<AiFillApi size={20} />}>Dupa</DangerButton>
+			<SuccessButton icon={<AiFillApi size={20} />}>Dupa</SuccessButton>
 			<main className="flex min-h-screen flex-col items-center justify-between">
 				index
 				<a href="/catalog">Products</a>

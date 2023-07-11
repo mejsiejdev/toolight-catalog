a6fea2ed30bd9e7abc3a1c46ff729643bb1a936a
Maciej Malinowski <mejsiejdev@gmail.com>
2023-07-11 13:40:05 +0200

    Added back the `Categories`

--- app/components/layout/navbar/components/menu/desktop/MenuDesktop.jsx
+++ app/components/layout/navbar/components/menu/desktop/MenuDesktop.jsx
@@ -1,8 +1,8 @@
-'use client';
-import Link from 'next/link';
-import './styles/menuDesktop.scss';
-import { useState } from 'react';
-import Categories from '@/app/components/layout/navbar/components/menu/desktop/components/Categories';
+"use client";
+import Link from "next/link";
+import "./styles/menuDesktop.scss";
+import { useState } from "react";
+import Categories from "@/app/components/layout/navbar/components/menu/desktop/components/Categories";
 
 const MenuDesktop = ({ items }) => {
   const [width, setWidth] = useState(0);
@@ -13,7 +13,7 @@ const MenuDesktop = ({ items }) => {
     width: `${width}px`,
     height: `${4}px`,
     opacity: `${opacity}`,
-    transition: '0.3s all',
+    transition: "0.3s all",
   };
   const mouseEnterItem = (e) => {
     const { width, x } = e.currentTarget.getBoundingClientRect();
@@ -32,9 +32,9 @@ const MenuDesktop = ({ items }) => {
     <div className="navbar__menu">
       <div className="menu-container">
         <ul className="menu">
-          {items.map((item) => {
-            return (
-              <li className="menu-item" key={crypto.randomUUID()}>
+          {items.map((item, key) =>
+            item.title !== "Katalog" ? (
+              <li className="menu-item" key={key}>
                 <Link
                   className="menu-item__link"
                   onMouseEnter={mouseEnterItem}
@@ -44,11 +44,36 @@ const MenuDesktop = ({ items }) => {
                   {item.title}
                 </Link>
               </li>
-            );
-          })}
+            ) : (
+              <Catalog key={key}>
+                <Link
+                  className="menu-item__link"
+                  onMouseEnter={mouseEnterItem}
+                  onMouseLeave={mouseLeaveItem}
+                  href={item.link}
+                >
+                  {item.title}
+                </Link>
+              </Catalog>
+            )
+          )}
           <span className="menu-hover" style={style}></span>
         </ul>
       </div>
+    </div>
+  );
+};
+
+const Catalog = ({ children }) => {
+  const [hover, setHover] = useState(false);
+  return (
+    <div
+      className="h-full"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <li className="menu-item">{children}</li>
+      {hover && <Categories />}
     </div>
   );
 };

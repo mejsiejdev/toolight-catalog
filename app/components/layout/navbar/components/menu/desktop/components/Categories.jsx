c3eacb5200575258aeb2cb72b6d5b6b0754d7b3a
Maciej Malinowski <mejsiejdev@gmail.com>
2023-07-12 09:00:01 +0200

    Remove imports from `Categories`
    They were unnecessary and ununsed

--- app/components/layout/navbar/components/menu/desktop/components/Categories.jsx
+++ app/components/layout/navbar/components/menu/desktop/components/Categories.jsx
@@ -1,30 +1,27 @@
-'use client';
-import './styles/categories.scss';
-import useGetCategories from '@/hooks/useGetCategories';
-import Link from 'next/link';
-import slugify from 'slugify';
-import { FiArrowRight } from 'react-icons/fi';
-import useWindowScroll from '@/hooks/useWindowScroll';
-import { useLayoutEffect, useRef, useState } from 'react';
+"use client";
+import "./styles/categories.scss";
+import useGetCategories from "@/hooks/useGetCategories";
+import Link from "next/link";
+import slugify from "slugify";
+import { FiArrowRight } from "react-icons/fi";
+import useWindowScroll from "@/hooks/useWindowScroll";
 
-const Categories = ({}) => {
-  const [open, setOpen] = useState(0);
-  const ref = useRef(null);
-  const { isLoading, error, categories } = useGetCategories('test');
+const Categories = () => {
+  const { isLoading, error, categories } = useGetCategories("test");
   const scroll = useWindowScroll();
   const handleArrow = (e) => {
-    e.currentTarget.classList.toggle('navbar__dropdown-link--active');
+    e.currentTarget.classList.toggle("navbar__dropdown-link--active");
   };
   const style = {
-    height: 'fit-content',
-    top: scroll > 1 ? '86px' : '100px',
-    transition: '0.3s',
+    height: "fit-content",
+    top: scroll > 1 ? "70px" : "100px",
+    transition: "0.3s",
   };
 
   return (
     <>
       {!isLoading && (
-        <div className="navbar__dropdown" ref={ref} style={style}>
+        <div className="navbar__dropdown" style={style}>
           {categories.map((category) => {
             return (
               <Link
@@ -32,12 +29,12 @@ const Categories = ({}) => {
                 onMouseEnter={handleArrow}
                 onMouseLeave={handleArrow}
                 href={`/${slugify(category, {
-                  replacement: '-',
+                  replacement: "-",
                   lower: true,
                 })}`}
                 key={crypto.randomUUID()}
               >
-                {category}{' '}
+                {category}{" "}
                 <FiArrowRight className="navbar__dropdown-link--arrow" />
               </Link>
             );
@@ -48,7 +45,7 @@ const Categories = ({}) => {
             className="navbar__dropdown-link navbar__dropdown-link--all"
             href="/wszystkie"
           >
-            Zobacz wszystkie{' '}
+            Zobacz wszystkie{" "}
             <FiArrowRight className="navbar__dropdown-link--arrow" />
           </Link>
         </div>

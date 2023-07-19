1ea685aaf5403656cf79b056197a004a9eae5ad7
Maciej Malinowski <mejsiejdev@gmail.com>
2023-07-19 10:18:44 +0200

    Update LogInButton.jsx

--- app/components/layout/navbar/components/buttons/LogInButton.jsx
+++ app/components/layout/navbar/components/buttons/LogInButton.jsx
@@ -1,14 +1,14 @@
-'use client';
-import { FiUser } from 'react-icons/fi';
-import './styles/logInButton.scss';
+import Link from "next/link";
+import { FiUser } from "react-icons/fi";
+import "./styles/logInButton.scss";
 
 const LogInButton = () => {
   return (
-    <div className="navbar__user">
+    <Link href={"/signin"} className="navbar__user">
       <button className="login-button">
         <FiUser className="login-icon" size={28} />
       </button>
-    </div>
+    </Link>
   );
 };
 

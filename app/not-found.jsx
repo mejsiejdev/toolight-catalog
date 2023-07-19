6e49e978570ff0002d9b5ecf6b0455ad58e02b21
Maciej Malinowski <mejsiejdev@gmail.com>
2023-07-19 10:18:39 +0200

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,9 +1,21 @@
-const NotHoundPage = () => {
+import Link from "next/link";
+
+const NotFound = () => {
   return (
-    <>
-      <h1>404</h1>
-    </>
+    <div className="flex flex-col gap-2 justify-center items-center min-h-screen">
+      <p className="text-2xl text-toolight-primary">404</p>
+      <p className="text-5xl font-semibold">Nie znaleziono strony</p>
+      <p className="text-toolight-secondary">
+        Przepraszamy, ale nie udało nam się znaleźć strony której szukasz.
+      </p>
+      <Link
+        href="/"
+        className="bg-toolight-primary hover:bg-toolight-primary-hover-dark transition py-2 px-4 font-semibold cursor-pointer flex flex-col items-center mt-6 rounded"
+      >
+        Powrót do strony głównej
+      </Link>
+    </div>
   );
 };
 
-export default NotHoundPage;
+export default NotFound;

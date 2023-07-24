38924cfa51a1566288352c4d67d60f2b2125f782
Maciej Malinowski <mejsiejdev@gmail.com>
2023-07-24 11:17:59 +0200

    Finished basic dashboard

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,9 +1,9 @@
-import Table from "./components/Table";
-import IconLink from "./components/IconLink";
+import Image from "next/image";
+import prisma from "@/dp";
 // Moment for date formatting
 import moment from "moment/moment";
 import "moment/locale/pl";
-import { MdEdit } from "react-icons/md";
+import Link from "next/link";
 
 const getLastModifiedProducts = async () => {
   const products = await prisma.products.findMany({
@@ -12,7 +12,7 @@ const getLastModifiedProducts = async () => {
         not: "",
       },
     },
-    take: 7,
+    take: 6,
     orderBy: {
       updatedAt: "desc",
     },
@@ -20,8 +20,24 @@ const getLastModifiedProducts = async () => {
   return products;
 };
 
+const getMissingProducts = async () => {
+  const products = await prisma.products.findMany({
+    where: {
+      title: {
+        not: "",
+      },
+      quantity: {
+        lt: 20,
+      },
+    },
+    take: 12,
+  });
+  return products;
+};
+
 const Admin = async () => {
-  const products = await getLastModifiedProducts();
+  const lastModifiedProducts = await getLastModifiedProducts();
+  const missingProducts = await getMissingProducts();
   return (
     <>
       <div className="flex flex-col gap-2">
@@ -30,24 +46,58 @@ const Admin = async () => {
           Wszystkie najnowsze zmiany i informacje znajdziesz tutaj.
         </h2>
       </div>
-      <div className="w-full grid grid-cols-2 gap-4">
-        <div className="w-full flex flex-col gap-4">
-          <h3 className="text-2xl text-toolight-secondary">
-            Ostatnio modyfikowane produkty
-          </h3>
-          <Table
-            headings={["Nazwa", "Zmodyfikowane", ""]}
-            rows={products.map((product, key) => [
-              product.title,
-              moment(product.updatedAt).locale("pl").fromNow(),
-              <div key={key} className="flex flex-row gap-2 justify-end">
-                <IconLink
-                  href={`/admin/products/${product.id}`}
-                  icon={<MdEdit />}
-                />
-              </div>,
-            ])}
-          />
+      <div className="w-full flex flex-col gap-4">
+        <h3 className="text-2xl text-toolight-secondary">
+          Ostatnio modyfikowane produkty
+        </h3>
+        <div className="grid grid-cols-6 gap-4">
+          {lastModifiedProducts.map((product, key) => (
+            <Link
+              href={`/admin/products/${product.id}`}
+              key={key}
+              className="shadow transition gap-2 flex flex-col bg-white rounded-lg border border-toolight-border-gray-light/50 hover:border-toolight-border-gray-light"
+            >
+              <Image
+                src={product.images[0]}
+                alt={product.title}
+                width="300"
+                height="300"
+                className="object-contain w-full aspect-square rounded-t-lg"
+              />
+              <div className="p-6 w-full flex flex-col gap-2">
+                <p>{product.title}</p>
+                <p className="text-toolight-secondary/75 text-sm">
+                  Zmodyfikowane {moment(product.updatedAt).fromNow()}
+                </p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="w-full flex flex-col gap-4">
+        <h3 className="text-2xl text-toolight-secondary">Brakujące produkty</h3>
+        <div className="grid grid-cols-6 gap-4">
+          {missingProducts.map((product, key) => (
+            <Link
+              href={`/admin/products/${product.id}`}
+              key={key}
+              className="shadow transition gap-2 flex flex-col bg-white rounded-lg border border-toolight-border-gray-light/50 hover:border-toolight-border-gray-light"
+            >
+              <Image
+                src={product.images[0]}
+                alt={product.title}
+                width="300"
+                height="300"
+                className="object-contain w-full aspect-square rounded-t-lg"
+              />
+              <div className="p-6 w-full flex flex-col gap-2">
+                <p>{product.title}</p>
+                <p className="text-toolight-secondary/75 text-sm">
+                  {`Pozostało ${product.quantity} sztuk produktu`}
+                </p>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </>

2a76767229d78d436b562c298c5f2f16159d3cfd
Maciej Malinowski <mejsiejdev@gmail.com>
2023-07-26 10:34:15 +0200

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col gap-2 justify-center items-center min-h-screen">
-      <p className="text-2xl text-toolight-primary">404</p>
+    <div className="flex flex-col gap-2 text-center justify-center min-h-screen items-center px-8">
+      <p className="text-7xl text-toolight-primary">404</p>
       <p className="text-5xl font-semibold">Nie znaleziono strony</p>
       <p className="text-toolight-secondary">
         Przepraszamy, ale nie udało nam się znaleźć strony której szukasz.

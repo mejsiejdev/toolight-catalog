f6ae0627a6c84276a8bf949d43c0bfb2d0068055
Maciej Malinowski <mejsiejdev@gmail.com>
2023-07-27 11:03:45 +0200

    Improve performance by adjusting images

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,11 @@ import Link from "next/link";
 import Baner from "@/public/assets/baner.jpg";
 import Wrapper from "./components/layout/Wrapper";
 
+export const metadata = {
+  title: "tooLight",
+  description: "Sprawdź najlepsze oświetlenie wewnętrzne w sklepie tooLight.",
+};
+
 const getFeatured = async () => {
   const featured = await prisma.products.findMany({
     where: {
@@ -22,19 +27,25 @@ const HomePage = async () => {
   return (
     <>
       <div className="flex w-full relative items-center justify-center">
-        <Image src={Baner} alt="Baner" className="h-[48rem] object-cover" />
+        <Image
+          priority
+          src={Baner}
+          alt="Baner"
+          sizes="100vw"
+          className="h-[48rem] w-full object-cover"
+        />
         <div className="absolute w-full h-full grid grid-cols-1 md:grid-cols-2">
           <div className="bg-toolight-secondary/80 flex flex-col items-center justify-center p-24"></div>
         </div>
         <Wrapper className={"absolute grid grid-cols-1 md:grid-cols-2"}>
           <div className="text-center md:text-left flex flex-col items-center md:items-start gap-8 text-white pl-8 xl:pl-0 pr-8 md:pr-24 py-24">
-            <p className="text-5xl lg:text-6xl xl:text-7xl">
+            <h2 className="text-5xl lg:text-6xl xl:text-7xl">
               Dla domu,
               <br /> dla łazienki, <br />
               <span className="font-semibold text-6xl lg:text-7xl xl:text-[5.25rem] md:leading-tight">
                 dla ciebie.
               </span>
-            </p>
+            </h2>
             <p className="text-xl lg:text-2xl">
               Otwarci na nowe możliwości, jesteśmy gotowi na potrzeby i sugestie
               naszych Klientów. Sprawdź naszą szeroką gamę produktów.
@@ -49,7 +60,7 @@ const HomePage = async () => {
         </Wrapper>
       </div>
       <Wrapper className="py-16 flex flex-col gap-8">
-        <h2 className="text-4xl">Polecane kategorie</h2>
+        <h3 className="text-4xl">Polecane kategorie</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 grid-flow-row lg:grid-cols-4 gap-x-4 gap-y-8 md:gap-8">
           {featured.map((product, key) => (
             <Link
@@ -60,9 +71,9 @@ const HomePage = async () => {
               <Image
                 src={product.images[1]}
                 alt={product.category}
-                width="1000"
-                height="1000"
-                className="aspect-square group-hover:brightness-110 transition object-contain -z-10 rounded-lg shadow"
+                width="400"
+                height="400"
+                className="w-full h-full aspect-square group-hover:brightness-110 transition object-contain -z-10 rounded-lg shadow"
               />
               <p className="text-black text-2xl font-medium group-hover:text-toolight-secondary/75 transition">
                 {product.category}

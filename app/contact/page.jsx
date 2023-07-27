872651af33aa7781cafb4ea4b5756b9345a19fa5
Maciej Malinowski <mejsiejdev@gmail.com>
2023-07-27 13:17:04 +0200

    Extract form into a component

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,15 +1,11 @@
-"use client";
-import { useId } from "react";
 import Wrapper from "../components/layout/Wrapper";
-import Input from "../components/Input";
-import SubmitButton from "../admin/components/SubmitButton";
+import Form from "./Form";
 
 export const metadata = {
-  title: "tooLight | Kontakt",
+  title: "Kontakt | tooLight",
 };
 
 const ContactPage = () => {
-  const id = useId();
   return (
     <Wrapper className="grid grid-cols-1 md:grid-cols-2 gap-8 py-8">
       <div className="flex flex-col gap-8">
@@ -21,7 +17,7 @@ const ContactPage = () => {
               Czynna od poniedziałku do piątku w godzinach 7.00 - 19.00.
             </p>
           </div>
-          <p className="text-toolight-primary font-semibold text-lg">
+          <p className="text-toolight-primary-hover-dark font-semibold text-lg">
             (+48) 857 337 777
           </p>
         </div>
@@ -31,7 +27,7 @@ const ContactPage = () => {
           </div>
           <a
             href="mailto:biuro@toolight.pl"
-            className="text-toolight-primary hover:text-toolight-primary-hover-dark hover:underline transition text-lg font-semibold"
+            className="text-toolight-primary-hover-dark hover:text-toolight-primary-hover-darker hover:underline transition text-lg font-semibold"
           >
             biuro@toolight.pl
           </a>
@@ -42,68 +38,13 @@ const ContactPage = () => {
           </div>
           <a
             href="mailto:reklamacje@toolight.pl"
-            className="text-toolight-primary hover:text-toolight-primary-hover-dark hover:underline transition text-lg font-semibold"
+            className="text-toolight-primary-hover-dark hover:text-toolight-primary-hover-darker hover:underline transition text-lg font-semibold"
           >
             reklamacje@toolight.pl
           </a>
         </div>
       </div>
-      <form
-        className="flex flex-col gap-6 md:gap-8"
-        action="https://toolight.pl/api/frontend/pl/contact"
-      >
-        <div className="flex flex-col gap-4">
-          <label
-            className="font-semibold text-toolight-secondary"
-            htmlFor={`${id}-fullname`}
-          >
-            Imię i nazwisko
-          </label>
-          <Input id={`${id}-fullname`} type="text" name="fullname" />
-        </div>
-        <div className="grid grid-cols-2 gap-6 md:gap-8">
-          <div className="flex flex-col gap-4">
-            <label
-              className="font-semibold text-toolight-secondary"
-              htmlFor={`${id}-email`}
-            >
-              Email
-            </label>
-            <Input id={`${id}-email`} type="email" name="email" />
-          </div>
-          <div className="flex flex-col gap-4">
-            <label
-              className="font-semibold text-toolight-secondary"
-              htmlFor={`${id}-telephone`}
-            >
-              Telefon
-            </label>
-            <Input id={`${id}-telephone`} type="text" name="telephone" />
-          </div>
-        </div>
-        <div className="flex flex-col gap-4">
-          <label
-            className="font-semibold text-toolight-secondary"
-            htmlFor={`${id}-subject`}
-          >
-            Temat
-          </label>
-          <Input id={`${id}-subject`} type="text" name="subject" />
-        </div>
-        <div className="flex flex-col gap-4">
-          <label
-            className="font-semibold text-toolight-secondary"
-            htmlFor={`${id}-body`}
-          >
-            Treść
-          </label>
-          <Input id={`${id}-body`} type="textarea" name="body" />
-        </div>
-        <input type="hidden" name="isPrivacyPoliticsCheckbox" value={true} />
-        <div className="flex flex-col items-end w-full md:w-auto">
-          <SubmitButton text={"Wyślij wiadomość"} />
-        </div>
-      </form>
+      <Form />
     </Wrapper>
   );
 };

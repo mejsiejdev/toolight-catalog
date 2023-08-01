b3c09de353411a3098dd95170024104153501dc6
Maciej Malinowski <mejsiejdev@gmail.com>
2023-08-01 13:49:49 +0200

    Update layout.jsx

--- app/(category)/[category]/layout.jsx
+++ app/(category)/[category]/layout.jsx
@@ -1,6 +1,7 @@
 import prisma from "@/dp";
 import { notFound } from "next/navigation";
 
+// Get all categories
 const getCategories = async () => {
   const products = await prisma.products.findMany();
   const categories = products.reduce((allProduct, product) => {
@@ -12,7 +13,9 @@ const getCategories = async () => {
 
 const Layout = async ({ children, params: { category } }) => {
   const categories = await getCategories();
+  // Check if the category parameter is valid
   if (!categories.includes(decodeURI(category)) && category !== "catalog") {
+    // If not, throw "Not Found" error.
     notFound();
   }
   return children;
